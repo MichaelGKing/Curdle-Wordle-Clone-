6245a4c2c7bbbe35c941b255c1b293e014d7c11b
warrun <34485299+warrun@users.noreply.github.com>
2022-05-11 19:16:21 +0800

    Took function out of class, tweaked comments

--- curdle/admin_login.py
+++ curdle/admin_login.py
@@ -7,16 +7,16 @@ from curdle import app
 # Regardless, there is a need for a login form for the administration interface
 # The administraion interface is used to upload new puzzles to the database
 
-# The class below inherits all functions from the FlaskForm imported from the WTForms package
+# The AdminLoginForm class inherits all functions from the FlaskForm imported from the WTForms package
 # This includes validators for inputs, so validation can be done server side
 
-class AdminLoginForm(FlaskForm):
+# This is a custom validator created following the WTForms official documentation
+# It returns an error if the password in entered incorrectly
+def validate_successful_auth(form, password):
+    if (password.data != app.config['SECRET_KEY']):
+        raise ValidationError('The password you entered in incorrect')
 
-    # This is a custom validator created following the WTForms official documentation
-    # It returns an error if the password in entered incorrectly
-    def validate_successful_auth(form, field):
-        if (field.data != app.config['SECRET_KEY']):
-            raise ValidationError('The password you entered in incorrect')
+class AdminLoginForm(FlaskForm):
     
     password = PasswordField('Password', validators=[DataRequired(), validate_successful_auth])
 
