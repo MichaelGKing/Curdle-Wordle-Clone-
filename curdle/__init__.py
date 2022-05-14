80db15998e232f8ee8407188d5028b9565f138b4
warrun <34485299+warrun@users.noreply.github.com>
2022-05-15 02:26:23 +0800

    Change application factory to work - Flask is now pointed to run the application at the curdle package - This has been changed from previous point to setup.py - Application factory now in a new function - Added database initialisation statements

--- curdle/__init__.py
+++ curdle/__init__.py
@@ -1,3 +1,6 @@
+# The __init__ file tells Flask that a folder should be treated as a package
+# 
+
 # When the curdle package is imported by 'setup.py' this file is run automatically because of its file name
 
 # From the python package flask, import the class Flask
@@ -6,17 +9,21 @@ from flask import Flask
 
 from config import Config
 
-# Create a new instance of the Flask application, called app
-
-app = Flask(__name__)
-
-# Set Flask configuration variables from Config Class variables
-
-app.config.from_object(Config)
-
 # Import our python module 'routes' which contains instructions what to render on what URL for the web site/application
 # The name of the folder this file is in is 'curdle' 
 # This means curdle is a python package, and we can import individual modules from it as needed
-
 from curdle import routes
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    from . import db
+
+    db.init_app(app)
+
+    return app
+
 
