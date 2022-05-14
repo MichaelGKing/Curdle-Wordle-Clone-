7e8f2914f59d583c08219b6b38b040e0354c58a4
warrun <34485299+warrun@users.noreply.github.com>
2022-05-15 03:03:51 +0800

    Fixed circular import error - Pointing Flask to the curdle package instead of setuo.py broke app - Removed application factory code from the function.. - All good now

--- curdle/__init__.py
+++ curdle/__init__.py
@@ -9,21 +9,17 @@ from flask import Flask
 
 from config import Config
 
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+from . import db
+
+db.init_app(app)
+
 # Import our python module 'routes' which contains instructions what to render on what URL for the web site/application
 # The name of the folder this file is in is 'curdle' 
 # This means curdle is a python package, and we can import individual modules from it as needed
-from curdle import routes
-
-def create_app():
-
-    app = Flask(__name__)
-
-    app.config.from_object(Config)
-
-    from . import db
-
-    db.init_app(app)
-
-    return app
+from curdle import routes   
 
 
