ec06457b04be76cfc98309a37f93107546069398
warrun <34485299+warrun@users.noreply.github.com>
2022-05-16 00:30:39 +0800

    Removed auth validator - Authentication is now managed within the /auth view function - Began preparing PuzzleUploadForm for database integration

--- curdle/forms.py
+++ curdle/forms.py
@@ -1,35 +1,34 @@
 from xmlrpc.client import Boolean
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, SubmitField, ValidationError, BooleanField
+from wtforms import StringField, PasswordField, SelectField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
-from curdle import app
+from curdle import db
+from werkzeug.security import check_password_hash
 
 # The game does not require users to login to save/track their play statistics.
 # Regardless, there is a need for a login form for the administration interface
 # The administraion interface is used to upload new puzzles to the database
 
 # The AdminLoginForm class inherits all functions from the FlaskForm imported from the WTForms package
-# This includes validators for inputs, so validation can be done server side
-
-# This is a custom validator created following the WTForms official documentation
-# It returns an error if the password in entered incorrectly
-def validate_successful_auth(form, password):
-    if (password.data != app.config['SECRET_KEY']):
-        raise ValidationError('The password you entered in incorrect')
+# This includes validators for inputs, so validation can be done server side   
 
 class AdminLoginForm(FlaskForm):
     
-    password = PasswordField('Password', validators=[DataRequired(), validate_successful_auth])
+    password = PasswordField('Password', validators=[DataRequired()])
 
     submit = SubmitField('Sign In')
 
     
 class PuzzleUploadForm(FlaskForm):
 
+    type_list = ['Soft', 'Hard', 'Brined', 'Processed', 'Fresh/Whey', 'Stretched Curd', 'Washed-rind', 'Blue']
+    country_list = ['England', 'USA', 'France', 'Switzerland']
+    animal_list = ['Cow', 'Sheep', 'Goat', 'Moose']
+
     name = StringField('Cheese Name', validators=[DataRequired()])
-    type = SelectField('Cheese Type', choices=['Soft', 'Hard', 'Brined', 'Processed', 'Fresh/Whey', 'Stretched Curd', 'Washed-rind', 'Blue'], validators=[DataRequired()])
-    country = SelectField('Country of Origin', choices=['England', 'USA', 'France', 'Switzerland'], validators=[DataRequired()])
-    animal = SelectField('Animal of Origin', choices=['Cow', 'Sheep', 'Goat', 'Moose'], validators=[DataRequired()])
+    type = SelectField('Cheese Type', choices=type_list, validators=[DataRequired()])
+    country = SelectField('Country of Origin', choices=country_list, validators=[DataRequired()])
+    animal = SelectField('Animal of Origin', choices=animal_list, validators=[DataRequired()])
     mould = BooleanField('Is mouldy?')
  
     submit = SubmitField('Upload Puzzle to Curdle Database')
