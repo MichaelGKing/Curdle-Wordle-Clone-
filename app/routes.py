9fbe8a8e2cd912e1c3ed8d9170906bdc18ba9264
warrun <34485299+warrun@users.noreply.github.com>
2022-05-18 00:59:42 +0800

    Added response to check-guess request hander

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
-from mimetypes import guess_type
-import os, json
-from urllib import response
+
+import os
 from app import app
 from app.forms import AdminLoginForm, PuzzleUploadForm
-from flask import render_template, flash, redirect, request, jsonify, Response
+from flask import render_template, flash, redirect, request
 from wtforms import ValidationError
 from werkzeug.security import check_password_hash
 from werkzeug.utils import secure_filename
