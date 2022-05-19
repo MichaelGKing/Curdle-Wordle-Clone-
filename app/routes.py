417fda72e24ce61844c2fe14c88e978f5abfc891
warrun <34485299+warrun@users.noreply.github.com>
2022-05-20 01:11:29 +0800

    View function updates - Removed testing auth functionality for puzzle upload page - Created add_puzzle function for use in other functions - Streamlined puzzle uploader (untested) - Completed guess checker (untested)

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 
-import os, csv
+import os, csv, json
 from app import app, db, models
 from app.forms import AdminLoginForm, PuzzleUploadForm
-from flask import render_template, flash, redirect, request
+from flask import jsonify, render_template, flash, redirect, request
 from wtforms import ValidationError
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from app.models import Cheese, Type, Country, Animal, Continent, Admin
 
-authorised = False
+# Used to index a puzzle for the day, should increment by +1 every day
+cheese_id_counter = 2
+# Somehow increment this every day - APScheduler can help?
+
 # Routes are written as shown below
 # The decorators at the beginning (starting with @app) define what URL's the code below them is run on
 # The view function contains this code
@@ -94,7 +97,7 @@ countries = [
     ["Venezuela", 7]
 ]
 
-# The current functionality reads puzzle list from a .csv file. 
+# The current functionality is to read aqeqwer puzzle list from a .csv file. 
 # This file is used to store initial puzzle list and any puzzles added using the administrators Puzzle Upload form.
 # If the server requires restarting and/or the database is lost or overwritten, puzzles will be reloaded from the last write to the csv
 # Puzzles must be set in following format to be loaded into the database:
@@ -146,7 +149,27 @@ for value in countries:
 
 # Deletes any existing values in the database for country
 models.Cheese.query.delete()
-# Iterates through the array of countries defined above and adds them to the database
+
+# Define a function to add a puzzle to the database, will be used in one of the view functions below
+def add_puzzle(puzzle):
+
+    if puzzle[4] == 'True' or puzzle[4] == '1':
+        is_mouldy = True
+    else:
+        is_mouldy = False
+
+    c = Cheese(
+        cheese_name=puzzle[0], 
+        type_id = db.session.query(Type.id).filter(Type.type == puzzle[1]).scalar(), 
+        animal_id = db.session.query(Animal.id).filter(Animal.animal_name == puzzle[2]).scalar(), 
+        country_id = db.session.query(Country.id).filter(Country.country_name == puzzle[3]).scalar(), 
+        mouldy=is_mouldy, 
+        image_filename=puzzle[5]
+        )
+
+    db.session.add(c)
+    db.session.commit()
+
 for value in cheeses:
 
     # The csv reader returns all values as strings
@@ -177,10 +200,6 @@ models.Admin.query.delete()
 p = Admin(password_hash=hash)
 db.session.add(p)
 db.session.commit()
-
-# Used to index a puzzle for the day, should increment by +1 every day
-cheese_id_counter = 1
-# Somehow increment this every day - APScheduler can help?
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -226,8 +245,6 @@ def auth():
 
         # Need to start a session for user login - old method did not work
         if check_password_hash(hash, password):
-            global authorised 
-            authorised = True
             flash('Administration Portal Authentication Successful')
             return redirect('/puzzle-uploader')
         else:   
@@ -240,37 +257,23 @@ def auth():
 @app.route('/puzzle-uploader', methods=['GET', 'POST']) 
 def puzzle_uploader():
     
-    global authorised
+    #global authorised
 
     # Create new instance of PuzzleUploadForm()
     form = PuzzleUploadForm()
 
     # Retreive lists of cheese attributes from the database
-    result = db.session.query(Type.type).all()
-    types = []
-    for row in result:
-        types.append(row[0])
 
-    result = db.session.query(Animal.animal_name).all() 
-    animals = []
-    for row in result:
-        animals.append(row[0])
-
-    result = db.session.query(Country.type).all() 
-    countries = []
-    for row in result:
-        countries.append(row[0])
-
-    form.set_types(types)
-    form.set_countries(animals)
-    form.set_animals(countries)
+    form.type.choices = db.session.query(Type.id, Type.type).all()
+    form.animal.choices = db.session.query(Animal.id, Animal.animal_name).all() 
+    form.country.choices = db.session.query(Animal.id, Country.country_name).all() 
 
     if form.validate_on_submit():
-
         # Here goes the code that recieves the data and handles adding it to the database
         if request.method == "POST":
 
             req = request.form
+
 
             f = form.image.data
             filename = secure_filename(f.filename)
@@ -282,11 +285,7 @@ def puzzle_uploader():
 
             # return redirect(request.url)
     
-    if authorised:
-        return render_template('puzzle-uploader.html', form=form)
-    
-    flash('You have not been authorised to view this page, please enter the admin password below to continue.')
-    return redirect('/auth')
+    return render_template('puzzle-uploader.html', form=form)
     
 @app.route('/check-guess', methods=['GET', 'POST'])
 def get_guess():
@@ -294,18 +293,42 @@ def get_guess():
     # Get cheese name from client side
     if request.method == "POST":
 
-        guess = request.get_json()
-        print(guess)
+        cheese_name = request.get_json() # This returns a single entry dict containing the cheese name
 
-    # Get list of cheese attributes from the database for the cheese
+        answer = Cheese.query.filter_by(id=cheese_id_counter).first()
 
-    # Check the guess against the correct cheese
+        guess = Cheese.query.filter_by(cheese_name=cheese_name['cheese_name']).first() #cheese_name
 
-    # Produce list of boolean values relatining to attribute matches 
+        results = {'name': True, 'country': True, 'mould': True, 'animal': True, 'type': True, 'continent': True}
 
-    # Return string in json format to the client side
+        # If the answer and guess share the same ID, the game is won (id is unique in the database)
+        if (answer.id == guess.id) and (answer.cheese_name != guess.cheese_name):
+                
+                return results
 
-        return {'message': "Hello Client!"}
+        if (answer.country_id != guess.country_id):
+            results["country"] = False
+
+        if (answer.mouldy != guess.mouldy):
+            results["mould"] = False
+
+        if (answer.animal_id != guess.animal_id):
+            results["name"] = False
+
+        if (answer.type_id != guess.type_id):
+            results["type"] = False
+
+
+        guess_country = Country.query.filter_by(id=guess.country_id).first()
+
+        answer_country = Country.query.filter_by(id=answer.country_id).first()
+
+        if (answer_country.continent_id != guess_country.continent_id):
+            results["continent"] = False
+        
+        return results
+
+        
 
 
 
