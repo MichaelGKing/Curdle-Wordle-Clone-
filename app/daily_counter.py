1449858868b5b0054e5717b8092e015ec582e985
warrun <34485299+warrun@users.noreply.github.com>
2022-05-21 02:17:26 +0800

    Daily puzzle id and date generator added - Complete and working - As far as I know

--- app/daily_counter.py
+++ app/daily_counter.py
@@ -1,7 +1,27 @@
-import datetime
+from datetime import datetime
 from app import app
 
-launch_time = app.config['LAUNCH_DATE']
-print(launch_time)
+# The date the application is 'launched' is set in the configuration variables
+# ie. launch_time = [2022, 5, 21, 0, 0] where [year, month, day, minute, seconds]
 
-start_counter = datetime.datetime()
+def get_daily_puzzle_id():
+    
+    ld = app.config['LAUNCH_DATE']
+
+    launch_date = datetime(ld[0], ld[1], ld[2], ld[3], ld[4])
+
+    todays_date = datetime.now()
+
+    return (todays_date - launch_date).days
+
+def get_daily_puzzle_info():
+    
+    ld = app.config['LAUNCH_DATE']
+
+    launch_date = datetime(ld[0], ld[1], ld[2], ld[3], ld[4])
+
+    todays_date = datetime.now()
+
+    puzzle_info = {'puzzle-id': (todays_date - launch_date).days, 'puzzle-date': todays_date}
+
+    return puzzle_info
