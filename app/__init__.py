550eaea74abbdcc050bfceeaf59f13359b3a204f
warrun <34485299+warrun@users.noreply.github.com>
2022-05-21 04:00:58 +0800

    Begun admin login management implementation

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager
 
 from app import models, routes
 
