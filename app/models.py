ca2ec7ebdf709f6edd266f704fb1aa76297975bb
warrun <34485299+warrun@users.noreply.github.com>
2022-05-22 23:58:32 +0800

    Added email column to user table

--- app/models.py
+++ app/models.py
@@ -55,6 +55,7 @@ class Cheese(db.Model, UserMixin):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
 class PuzzleHistory(db.Model):
