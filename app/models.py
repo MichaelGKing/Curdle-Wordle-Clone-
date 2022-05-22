9efac942cb42a2482a88a283ab554b8386b79fc6
warrun <34485299+warrun@users.noreply.github.com>
2022-05-22 23:13:37 +0800

    Multiple changes to models.py  Added imports and functions for user logins  Made User class a child of UserMixin  Added set and check password fuctions to User class

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 import csv
+from flask_login import UserMixin
 
 from sqlalchemy import ForeignKey
-from app import db, app
-from werkzeug.security import generate_password_hash
+from app import db, app, login
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Type(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,10 +47,17 @@ class Cheese(db.Model):
     def __repr__(self):
         return '<Cheese: {}>'.format(self.cheese_name)   
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+         self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+         return check_password_hash(self.password_hash, password)
 
 class PuzzleHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -77,4 +86,9 @@ def add_puzzle(puzzle):
         )
 
     db.session.add(c)
-    db.session.commit()
+    db.session.commit
+
+# Configure a user loader function - Loads a user, given an ID, from the database, and registers it with Flask-Login
+@login.user_loader
+def load_user(id):
+     return User.query.get(int(id))
